179127083b19e2c971d212f8ebd197403287242b
Uriel Guzmán <uriel@aromadigital.mx>
2019-03-26 15:32:57 -0600

    Modifique algo

--- fullTable.js
+++ fullTable.js
@@ -12,7 +12,7 @@ const client = new Client({
 
 client.connect()
 .then(() => console.log("Connected succesfully"))
-.then(() => client.query("insert into empleados values ($1, $2)", [123, 'David']))
+.then(() => client.query("insert into libros values ($1, $2)", [17283], 'David']))
 .then(() => client.query("select * from empleados"))
 .then(results => console.table(results.rows))
 .catch(e => console.log(e))
